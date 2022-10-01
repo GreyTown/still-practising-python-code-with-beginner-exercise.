1ac81a1b0d20cd1fd0bac6ffcd8774cb1068815f
Aggrey Kanyoro <67312531+GreyTown@users.noreply.github.com>
2022-10-01 10:31:00 +0300

    Add files via upload
    Created a simple madlibs using the f string in python

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+# a madlibs program created with f string in python
+
+storyTeller = 'Kanyoro	'
+village = 'Kasese'
+brotherName = 'Arthur'
+sisterName = 'Robby'
+years_in_village = '18'
+favCrop = 'Potatoes'
+
+
+story = f"{storyTeller} lives in the eastern part of {village} where he has stayed with his brother {brotherName} and sister {sisterName} for the last {years_in_village} years growing {favCrop} because they make them healthy."
+print(story)
